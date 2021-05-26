45439b2a1296f86a42332aa28f6808ae420d2c8d
khokon9363 <khokon9363@gmail.com>
2021-05-26 11:36:33 +0600

    Bug fixed

--- js/script.js
+++ js/script.js
@@ -9,8 +9,15 @@ function output() {
 }
 
 playground.addEventListener("paste", function(e) {
-    e.preventDefault()
-    var text = e.clipboardData.getData("text/plain")
-    playground.textContent += text
+    // cancel paste
+    e.preventDefault();
+
+    // get text representation of clipboard
+    var text = (e.originalEvent || e).clipboardData.getData('text/plain');
+
+    // insert text manually
+    document.execCommand("insertHTML", false, text);
+
+    // call the output function
     output()
 })
